2648fe0a8535ae0f1a55ca3761a4e537882db3b0
AnAfterthought <chngweicheng@gmail.com>
2025-01-17 17:39:40 +0800

    Added add and list features

--- src/main/java/BPlusChatter.java
+++ src/main/java/BPlusChatter.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 public class BPlusChatter {
+
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int index = 0;
+
         Scanner userInputScanner = new Scanner(System.in);
         String userInput;
         String greeting = "Hello! I'm BPlusChatter :)\n\tWhat can I do for you?";
@@ -10,6 +14,7 @@ public class BPlusChatter {
         System.out.println("\t" + separator);
         System.out.println("\t" + greeting);
         System.out.println("\t" + separator);
+
         while (true) {
             userInput = userInputScanner.nextLine();
             if (userInput.equals("bye")) {
@@ -18,9 +23,19 @@ public class BPlusChatter {
                 System.out.println("\t" + separator);
                 break;
             }
-            System.out.println("\t" + separator);
-            System.out.println("\t" + userInput);
-            System.out.println("\t" + separator);
+            if (userInput.equals("list")) {
+                System.out.println("\t" + separator);
+                for (int i = 0; i < index; i++) {
+                    System.out.println("\t"+ (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("\t" + separator);
+            } else {
+                tasks[index] = userInput;
+                index += 1;
+                System.out.println("\t" + separator);
+                System.out.println("\tadded: " + userInput);
+                System.out.println("\t" + separator);
+            }
         }
     }
 }
