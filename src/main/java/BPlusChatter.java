7161acdea564cbf7824651bc9fa069ffad9ea1e5
AnAfterthought <chngweicheng@gmail.com>
2025-01-17 15:46:39 +0800

    Add greeting and exit

--- src/main/java/BPlusChatter.java
+++ src/main/java/BPlusChatter.java
@@ -1,5 +1,12 @@
 public class BPlusChatter {
     public static void main(String[] args) {
-        
+        String greeting = "Hello! I'm BPlusChatter\nWhat can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        String separator = "____________________________________________________________";
+        System.out.println(separator);
+        System.out.println(greeting);
+        System.out.println(separator);
+        System.out.println(exit);
+        System.out.println(separator);
     }
 }
