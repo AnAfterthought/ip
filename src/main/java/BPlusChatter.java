d876d6ed2472f0e3b0199f209f307b5c87855eae
AnAfterthought <chngweicheng@gmail.com>
2025-01-17 17:57:54 +0800

    Add task feature now uses Task objects

--- src/main/java/BPlusChatter.java
+++ src/main/java/BPlusChatter.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BPlusChatter {
 
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int index = 0;
 
         Scanner userInputScanner = new Scanner(System.in);
@@ -26,14 +26,15 @@ public class BPlusChatter {
             if (userInput.equals("list")) {
                 System.out.println("\t" + separator);
                 for (int i = 0; i < index; i++) {
-                    System.out.println("\t"+ (i + 1) + ". " + tasks[i]);
+                    System.out.println("\t"+ (i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i]);
                 }
                 System.out.println("\t" + separator);
             } else {
-                tasks[index] = userInput;
+                Task task = new Task(userInput);
+                tasks[index] = task;
                 index += 1;
                 System.out.println("\t" + separator);
-                System.out.println("\tadded: " + userInput);
+                System.out.println("\tadded: " + task);
                 System.out.println("\t" + separator);
             }
         }
