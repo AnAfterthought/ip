710f01e600a1fe7ade8c47c07e6b3233a5bb9351
AnAfterthought <chngweicheng@gmail.com>
2025-01-26 12:57:49 +0800

    Add support for mark, unmark and delete command in Parser class

--- src/main/java/InvalidUnmarkException.java
+++
@@ -1,7 +0,0 @@
-public class InvalidUnmarkException extends Exception {
-
-    public String toString(int count) {
-        return "\tWRONG FORMAT :(\n " + "\tFormat: unmark <task number>\n" +
-                "\tYou have " + count + " task(s)";
-    }
-}
